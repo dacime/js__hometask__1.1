841bf2b6f696a62c4162eb778418a6fd5499a858
Alexey Toropov <toropov.alexej@protonmail.com>
2021-06-08 18:26:44 +0300

    First hmtsk
    My first hometask is ready!

---
+++ js_1.1.js
@@ -0,0 +1,19 @@
+//Температура
+
+var Tc = prompt("Введитие темературу по цельсию:");
+var Tf = (9 / 5) * Tc + 32;
+alert(Tf);
+//Присвоение
+
+var admin;
+var name = "Василий";
+admin = name;
+alert(admin);
+
+//Сложение числа и строки
+
+var a = 1000;
+var b = "108";
+document.write("Число, полученное в результате вычисления:")
+document.write(a + b);
+//Ответ: 1000108
